dea0ba85b80778f337fe7f0a334da6b48957a958
Bogdan Bondics <bogdybondics@outlook.com>
2021-06-16 09:38:15 +0300

    Add project files.

---
+++ Bondics_Bogdan_Project_SPE/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Cal_Sah
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.grid = new System.Windows.Forms.DataGridView();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.timer2 = new System.Windows.Forms.Timer(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			this.restartbut = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// grid
+			// 
+			this.grid.AllowUserToAddRows = false;
+			this.grid.AllowUserToDeleteRows = false;
+			this.grid.AllowUserToResizeColumns = false;
+			this.grid.AllowUserToResizeRows = false;
+			this.grid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.grid.ColumnHeadersVisible = false;
+			this.grid.Location = new System.Drawing.Point(13, 80);
+			this.grid.MultiSelect = false;
+			this.grid.Name = "grid";
+			this.grid.ReadOnly = true;
+			this.grid.RowHeadersVisible = false;
+			this.grid.ScrollBars = System.Windows.Forms.ScrollBars.None;
+			this.grid.Size = new System.Drawing.Size(669, 474);
+			this.grid.TabIndex = 0;
+			this.grid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid_CellClick);
+			this.grid.SelectionChanged += new System.EventHandler(this.grid_SelectionChanged);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 300;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// timer2
+			// 
+			this.timer2.Enabled = true;
+			this.timer2.Interval = 1000;
+			this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label1.Location = new System.Drawing.Point(526, 36);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(129, 20);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Timp: 0 min 0 s";
+			// 
+			// restartbut
+			// 
+			this.restartbut.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.restartbut.Location = new System.Drawing.Point(13, 13);
+			this.restartbut.Name = "restartbut";
+			this.restartbut.Size = new System.Drawing.Size(116, 30);
+			this.restartbut.TabIndex = 2;
+			this.restartbut.Text = "Restart Game";
+			this.restartbut.UseVisualStyleBackColor = true;
+			this.restartbut.Click += new System.EventHandler(this.restartbut_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(694, 566);
+			this.Controls.Add(this.restartbut);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.grid);
+			this.Name = "Form1";
+			this.Text = "Bondics_Bogdan_Project_SPE";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button restartbut;
+    }
+}
+
---
+++ Bondics_Bogdan_Project_SPE/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Cal_Sah
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool start = true;
+        int lastrand, lastcol, timp = 0, casute = 1, sec = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            grid.ColumnCount = 8;
+            grid.RowCount = 8;
+            foreach (DataGridViewColumn c in grid.Columns)
+                c.Width = grid.Width / 8;
+            foreach(DataGridViewRow c in grid.Rows)
+                c.Height = grid.Height/ 8;
+            grid.Height++;
+            for(int i=0;i<8;i++)
+            {
+                for(int j=0;j<8;j++)
+                {
+                    grid[i, j].Style.ForeColor = Color.DarkGray;
+                    grid[i, j].Style.BackColor = Color.LightGray;
+                }
+            }
+            restartbut.Visible = false;
+        }
+
+        private void grid_SelectionChanged(object sender, EventArgs e)
+        {
+            grid.ClearSelection();
+        }
+
+        private void grid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rand = e.RowIndex,colo = e.ColumnIndex;
+            if (start)
+            {
+                cal(colo, rand);
+                start = false;
+                lastcol = colo;
+                lastrand = rand;
+                grid[colo, rand].Style.BackColor = Color.Blue;
+            }
+            else
+            {
+                if(grid[colo,rand].Style.BackColor!=Color.Yellow)
+                {
+                    MessageBox.Show("Casuta invalida");
+                }
+                else
+                {
+                    recal(lastcol, lastrand);
+                    lastcol = colo;
+                    lastrand = rand;
+                    if (cal(colo, rand) == false)
+                    {
+                        grid[colo, rand].Style.BackColor = Color.Red;
+                        MessageBox.Show("Ai pierdut");
+                        timer2.Enabled = false;
+                        restartbut.Visible = true;
+                    }
+                    else {
+                        grid[colo, rand].Style.BackColor = Color.Blue;
+                        casute++;
+                            }
+                    if (casute == 64)
+                    {
+                        MessageBox.Show("Bravooo!");
+                        MessageBox.Show("UR the BEST!!");
+                        MessageBox.Show("Te pupa smecheru barosan");
+                        timer2.Enabled = false;
+                        restartbut.Visible = true;
+                    }
+                }
+            }
+        }
+
+        private void restartbut_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(Application.ExecutablePath);
+            Close();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            sec++;
+            label1.Text ="Timp: " + (sec / 60).ToString() + " min " + (sec % 60).ToString() + " s ";
+            
+        }
+
+        private bool cal(int i,int j)
+        {
+            bool liber = false;
+            try
+            {
+                if (grid[i + 1, j + 2].Style.BackColor != Color.Blue)
+                {
+                    grid[i + 1, j + 2].Style.BackColor = Color.Yellow;
+                    liber = true;
+                }
+            }
+            catch { }
+            try
+            {
+                if (grid[i - 1, j + 2].Style.BackColor != Color.Blue)
+                {
+                    grid[i - 1, j + 2].Style.BackColor = Color.Yellow;
+                    liber = true;
+                }
+            }
+            catch { }
+            try
+            {
+                if (grid[i + 2, j + 1].Style.BackColor != Color.Blue)
+                {
+                    grid[i + 2, j + 1].Style.BackColor = Color.Yellow;
+                    liber = true;
+                }
+            }
+            catch { }
+            try
+            {
+                if (grid[i + 2, j - 1].Style.BackColor != Color.Blue)
+                {
+                    grid[i + 2, j - 1].Style.BackColor = Color.Yellow;
+                    liber = true;
+                }
+            }
+            catch { }
+            try
+            {
+                if (grid[i + 1, j - 2].Style.BackColor != Color.Blue)
+                {
+                    grid[i + 1, j - 2].Style.BackColor = Color.Yellow;
+                    liber = true;
+                }
+            }
+            catch { }
+            try
+            {
+                if (grid[i - 1, j - 2].Style.BackColor != Color.Blue)
+                {
+                    grid[i - 1, j - 2].Style.BackColor = Color.Yellow;
+                    liber = true;
+                }
+                }
+            catch { }
+            try
+            {
+                if (grid[i - 2, j + 1].Style.BackColor != Color.Blue)
+                {
+                    grid[i - 2, j + 1].Style.BackColor = Color.Yellow;
+                    liber = true;
+                }
+                }
+            catch { }
+            try
+            {
+                if (grid[i - 2, j - 1].Style.BackColor != Color.Blue)
+                {
+                    grid[i - 2, j - 1].Style.BackColor = Color.Yellow;
+                    liber = true;
+                }
+                }
+            catch { }
+            return liber;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timp++;
+            if(timp==1)
+                MessageBox.Show("Alege un punct de incepere");
+
+        }
+
+        private void recal(int i,int j)
+        {
+            try
+            {
+                if (grid[i + 1, j + 2].Style.BackColor != Color.Blue)
+                    grid[i + 1, j + 2].Style.BackColor = Color.LightGray;
+            }
+            catch { }
+            try
+            {
+                if (grid[i - 1, j + 2].Style.BackColor != Color.Blue)
+                    grid[i - 1, j + 2].Style.BackColor = Color.LightGray;
+            }
+            catch { }
+            try
+            {
+                if (grid[i + 2, j + 1].Style.BackColor != Color.Blue)
+                    grid[i + 2, j + 1].Style.BackColor = Color.LightGray;
+            }
+            catch { }
+            try
+            {
+                if (grid[i + 2, j - 1].Style.BackColor != Color.Blue)
+                    grid[i + 2, j - 1].Style.BackColor = Color.LightGray;
+            }
+            catch { }
+            try
+            {
+                if (grid[i + 1, j - 2].Style.BackColor != Color.Blue)
+                    grid[i + 1, j - 2].Style.BackColor = Color.LightGray;
+            }
+            catch { }
+            try
+            {
+                if (grid[i - 1, j - 2].Style.BackColor != Color.Blue)
+                    grid[i - 1, j - 2].Style.BackColor = Color.LightGray;
+            }
+            catch { }
+            try
+            {
+                if (grid[i - 2, j + 1].Style.BackColor != Color.Blue)
+                    grid[i - 2, j + 1].Style.BackColor = Color.LightGray;
+            }
+            catch { }
+            try
+            {
+                if (grid[i - 2, j - 1].Style.BackColor != Color.Blue)
+                    grid[i - 2, j - 1].Style.BackColor = Color.LightGray;
+            }
+            catch { }
+        }
+    }
+}
